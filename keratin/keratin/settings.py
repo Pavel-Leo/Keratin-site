985c1ee61ddd7bb2ea80dad692b60361979af106
Павел Леонтьев <fionit-2007@mail.ru>
2023-03-11 03:26:04 +0300

    Change readme info

--- keratin/keratin/settings.py
+++ keratin/keratin/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
 STATIC_URL = "/static/"
 
 CACHES = {
