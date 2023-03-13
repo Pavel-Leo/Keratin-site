2a1dd5ac52ab5b454513596ad61f7b67c9317c7a
Павел Леонтьев <fionit-2007@mail.ru>
2023-03-13 21:55:35 +0300

    Add config settings

--- keratin/keratin/settings.py
+++ keratin/keratin/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from core import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "$bc(9%2)#xlcz6ej+st6i=)sggam*rrh#4b_cn9%g0#re%lfqv"
+SECRET_KEY = config.KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
